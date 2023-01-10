345a8a205b92560e181d7147ac92ba1d79e2ac92
opesnoir <goodsurroundsme@outlook.com>
2023-01-10 17:05:03 +0100

    routing structuur geimplementeerd, lege componenten aangemaakt ook voor nav en footer, routes gedefinieerd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/nav/Nav";
+import Footer from "./components/footer/Footer";
+import {Navigate, Route, Routes} from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import NotFound from "./pages/notFound/NotFound";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
 
 function App() {
+  const auth = false;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <>
+        <Nav/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/login-profile" element={auth === true ? <Profile/> : <Navigate to="/"/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+        <Footer/>
+      </>
   );
 }
 
