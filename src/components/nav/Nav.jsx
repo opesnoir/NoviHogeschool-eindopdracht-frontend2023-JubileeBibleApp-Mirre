66ad222b653958ac3e11f0a1a2bdfd15fb7f07f9
opesnoir <goodsurroundsme@outlook.com>
2023-01-10 21:05:24 +0100

    nav gemaakt en gestyled, inclusief logo

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
+import logo from '../../assets/jubilee-bibleapp-high-resolution-logo-white-on-transparent-background copy.png'
+import {AiOutlineUser} from 'react-icons/ai';
+import {AiOutlineUserAdd} from 'react-icons/ai';
+import styles from './Nav.module.css';
 
-const Nav = () => {
+const Nav = ({auth}) => {
+
+    const navLink = ({isActive}) => isActive
+        ? 'active-link'
+        : 'default-link'
+
     return (
-        <div>
-
-        </div>
+        <>
+            <nav>
+                <div className={styles.outerContainer}>
+                <ul>
+                    <li><NavLink to="/" className={navLink}>Home</NavLink></li>
+                    <li><NavLink to="/about" className={navLink}>Over</NavLink></li>
+                    <img src={logo} alt="Jubilee BibleApp logo" className={styles.logo}/>
+                    <li><NavLink to="/register" className={navLink}><AiOutlineUserAdd/>Registreer</NavLink></li>
+                    <li><NavLink to="/login" className={navLink}><AiOutlineUser/>Login</NavLink></li>
+                    {auth && <li><NavLink to="/login-profile" className={navLink}>Profielpagina</NavLink></li>}
+                </ul>
+                </div>
+            </nav>
+        </>
     );
 };
 
