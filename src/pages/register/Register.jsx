8791275e6959cfd4c35df7d54cc2b0ebf45591cc
opesnoir <goodsurroundsme@outlook.com>
2023-01-11 17:53:08 +0100

    registratiepagina gemaakt

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {useForm} from "react-hook-form";
 import BasicIntro from "../../components/BasicIntro/BasicIntro";
 import FormInput from "../../components/FormInput/FormInput";
@@ -6,10 +6,34 @@ import styles from "../login/Login.module.css";
 import Button from "../../components/Button/Button";
 import Image from "../../components/Image/Image";
 import schapen from "../../assets/register-schapen-pexels-trinity-kubassek-288621 (1) copy.jpg";
+import axios from "axios";
 
 const Register = () => {
-    const {register, formState: {errors}} = useForm();
+    const {register, watch, formState: {errors}} = useForm();
+    const userPassword = useRef({}) // returns current
+    userPassword.current = watch("password", " ");
+
     const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+
+    const [email, setEmail] = useState(' ')
+    const [name, setName] = useState(' ')
+    const [password, setPassword] = useState(' ')
+
+    // form submission call
+    function handleSubmit(event) {
+        event.preventDefault()
+    }
+
+    //send Post request (JSON methode)
+    axios.post('/api/save', {name, email, password})
+        .then((response) => {
+            localStorage.setItem('formData', JSON.stringify({name, email, password}));
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
 
     return (
         <>
@@ -20,50 +44,63 @@ const Register = () => {
                 link="/login"
                 title="Registreer"
             >
-                {<form action="">
+
+                {<form onSubmit={handleSubmit}>
+                    <FormInput
+                        id="name"
+                        name="name"
+                        type="text"
+                        role="user"
+                        onChange={event => setName(event.target.value)}
+                        placeholder="Naam:"
+                        register={register}
+                        errors={errors}
+                        validationSchema={{
+                            required: "Naam is vereist"
+                        }}
+                        required
+                    />
+
                     <FormInput
                         id="email"
                         name="email"
                         type="email"
                         role="user"
-                        onChange=" "
+                        onChange={event => setEmail(event.target.value)}
                         placeholder="Email:"
                         register={register}
                         errors={errors}
                         validationSchema={{
-                            required: "Email is vereist",
-                            pattern: {
+                            required: "Email is vereist", pattern: {
                                 value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
                                 message: "Geldig emailadres is vereist"
                             }
                         }}
                         required
                     />
+
                     <FormInput
                         id="password"
                         name="password"
                         type={showPassword ? "text" : "password"}
                         role="user"
-                        onChange=" "
+                        onChange={event => setPassword(event.target.value)}
                         placeholder="Wachtwoord:"
                         register={register}
                         errors={errors}
                         validationSchema={{
                             required: {
-                                value: true,
-                                message: "Wachtwoord is vereist"
-                            },
-                            pattern: {
+                                value: true, message: "Wachtwoord is vereist"
+                            }, pattern: {
                                 value: /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
                                 message: "Het wachtwoord bevat minimaal 8 tekens, waarvan 1 hoofdletter, 1 getal en 1 leesteken"
-                            },
-                            minLength: {
-                                value: 8,
-                                message: "Voer minimaal 8 tekens in"
+                            }, minLength: {
+                                value: 8, message: "Voer minimaal 8 tekens in"
                             }
                         }}
                         required
                     />
+
                     <label>
                         <input
                             className={styles.passwordCheckbox}
@@ -73,6 +110,33 @@ const Register = () => {
                         />
                         Wachtwoord tonen
                     </label>
+
+                    <FormInput
+                        id="confirmpassword"
+                        name="confirmpassword"
+                        type={showConfirmPassword ? "text" : "password"}
+                        role="user"
+                        placeholder="Bevestig het wachtwoord:"
+                        register={register}
+                        errors={errors}
+                        validationSchema={{
+                            required: {
+                                value: true, message: "Bevestiging is vereist"
+                            }, validate: value => value === password.current,
+                        }}
+                        required
+                    />
+
+                    <label>
+                        <input
+                            className={styles.passwordCheckbox}
+                            type="checkbox"
+                            checked={showConfirmPassword}
+                            onChange={() => setShowConfirmPassword(!showConfirmPassword)}
+                        />
+                        Bevestiging tonen
+                    </label>
+
                     <div>
                         <Button
                             type="button"
@@ -80,6 +144,7 @@ const Register = () => {
                         />
                     </div>
                 </form>}
+
             </BasicIntro>
             <Image
                 src={schapen}
