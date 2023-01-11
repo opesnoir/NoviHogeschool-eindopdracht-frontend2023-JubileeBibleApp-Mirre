c863399eed1e4ff4a8bc05ceb3b94508bf0535ae
opesnoir <goodsurroundsme@outlook.com>
2023-01-11 11:39:46 +0100

    page not found gemaakt (met componenten), timeout voor3 seconden gezet)

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import BasicSection from "../../components/BasicSection/BasicSection";
+import Image from "../../components/Image/Image";
+import ram from "../../assets/pagenotfound-ram-pexels-johannes-havn-3218340 copy.jpg";
+import {useNavigate} from "react-router-dom";
 
 const NotFound = () => {
-    return (
-        <div>
 
-        </div>
+    const navigate = useNavigate();
+    setTimeout(() => {
+        navigate(-1)
+    }, 3000)
+
+    return (
+        <>
+            <BasicSection
+                title="404 | Pagina niet gevonden"
+                paragraph2="De pagina die je zoekt bestaat niet. Je wordt binnen 3 seconden teruggestuurd naar de vorige pagina"
+            />
+            <Image
+                src={ram}
+                alt="Afbeelding van een ram"
+            />
+        </>
     );
 };
 
