d9ed412ee72b38a88611b7076085d22564f8f0b5
opesnoir <goodsurroundsme@outlook.com>
2023-01-11 14:17:54 +0100

    component forminput gemaakt, voor de registration en login formulieren

---
+++ src/components/FormInput/FormInput.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styles from "./FormInput.module.jsx.css"
+
+const FormInput = ({
+                      htmlFor,
+                      label,
+                      required,
+                      type,
+                      id,
+                      name,
+                      role,
+                      event,
+                      placeholder,
+                      validationSchema,
+                      register,
+                      errors
+                  }) => {
+
+    return (
+        <>
+            <div className={styles.container}>
+                <label htmlFor={htmlFor}>
+                    {label}
+                    {required}
+                </label>
+                <input
+                    type={type}
+                    id={id}
+                    name={name}
+                    role={role}
+                    onChange={event}
+                    placeholder={placeholder}
+                    {...register(name, validationSchema)}
+                />
+                {errors && errors[name]?.type === "required" && (
+                    <span className={styles.errorsTekst}>{errors[name]?.message} </span>
+                )}
+                {errors && errors[name]?.type === "pattern" && (
+                    <span className={styles.errorsTekst}>{errors[name]?.message} </span>
+                )}
+                {errors && errors[name]?.type === "minLength" && (
+                    <span className={styles.errorsTekst}>{errors[name]?.message} </span>
+                )}
+            </div>
+        </>
+    );
+};
+
+export default FormInput;
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from "../../components/Button/Button";
 
 const Login = () => {
     return (
         <div>
-
+            <Button
+                type="button"
+                name="verzend"
+            />
         </div>
     );
 };
