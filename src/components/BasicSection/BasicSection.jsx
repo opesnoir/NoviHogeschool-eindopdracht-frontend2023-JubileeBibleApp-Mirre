764d0786dc70fbfb07ae169e50aabc5a9331de5f
opesnoir <goodsurroundsme@outlook.com>
2023-01-11 11:19:44 +0100

    component voor basicsection gemaakt, ik gebruik namelijk de lay-out en styling van  de about pagina ook voor de page not found pagina. Verder de about.jsx en about.css die er al stond aangepast/verwijderd

---
+++ src/components/BasicSection/BasicSection.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from "./BasicSection.module.css"
+
+const BasicSection = ({title, paragraph1, paragraph2, name}) => {
+    return (
+            <section>
+                <article className={styles.outerContainer}>
+                    <div className={styles.innerContainer}>
+                        <div>
+                            <h1 className={styles.title}>{title}</h1>
+                        </div>
+                        <div>
+                            <p className={styles.p}>{paragraph1}</p>
+                            <p className={styles.p}>{paragraph2}</p>
+                            <p className={styles.name}>{name}</p>
+                        </div>
+                    </div>
+                </article>
+            </section>
+    );
+};
+
+export default BasicSection;
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import schaap from '../../assets/about-sheep-pexels-arthur-swiffen-12550240 copy.jpg'
+import Image from '../../components/Image/Image';
+import BasicSection from "../../components/BasicSection/BasicSection";
 
 const About = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <BasicSection
+                title="Over mij"
+                paragraph1="“Uw woord is een lamp voor mijn voet, en een licht op mijn pad.”
+                                (Psalm 119:105). Dat
+                                schreef David om het woord van de HEERE te omschrijven. Voor mij betekent Gods woord
+                                hetzelfde. Daarom heb ik ervoor gekozen voor mijn afstudeer opdracht deze
+                                Bijbelapplicatie te bouwen."
+                paragraph2="Mijn geloof in JHWH vormt het fundament van mijn bestaan en Zijn
+                                woord is mijn morele
+                                kompas. Ik hoop dan ook dat de Bijbel jou net zo zal inspireren en dat het een licht
+                                voor jouw voeten mag zijn."
+                name="Mirre"
+            />
+            <Image
+                src={schaap}
+                alt="Afbeelding van een schaap"
+            />
+        </>
     );
 };
 
